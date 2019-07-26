60c5980cc072c16a8e2fd883ec0c62a0937f7d65
Michael Telford <michael.telford@live.com>
2019-07-26 21:24:12 +1000

    Added wgit extension for all_links in a Document

--- lib/broken_link_finder.rb
+++ lib/broken_link_finder.rb
@@ -1,2 +1,3 @@
+require_relative "./broken_link_finder/wgit_extensions"
 require_relative "./broken_link_finder/version"
 require_relative "./broken_link_finder/finder"
---
+++ lib/broken_link_finder/wgit_extensions.rb
@@ -0,0 +1,19 @@
+require 'wgit'
+
+# We pull out all of a Document's links, not just the links to other webpages.
+Wgit::Document.define_extension(
+  :all_links,
+  '//*/@href | //*/@src',
+  singleton: false,
+  text_content_only: true,
+) do |links|
+  if links
+    links.map! do |link|
+      Wgit::Url.new(link)
+    rescue
+      nil
+    end
+    links.compact!
+  end
+  links
+end
